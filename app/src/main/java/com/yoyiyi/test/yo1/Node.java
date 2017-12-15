27118752dd68c7583ce880beefc14be05da8b6aa
zzq <2455676683@qq.com>
2017-12-15 16:41:19 +0800

    yoyiyi

---
+++ app/src/main/java/com/yoyiyi/test/yo1/Node.java
@@ -0,0 +1,44 @@
+package com.yoyiyi.test.yo1;
+
+/**
+ * 类名：Node
+ * 描述：TODO
+ * 时间：2017/12/15 16:20
+ *
+ * @author ZhongZhengQuan
+ * @version 1.0
+ */
+public class Node {
+
+    public void note() {
+        /**
+         * 1.Canvas
+         *          1.范围裁切
+         *          2.几何变换
+         *
+         *
+         *2.Start
+         *         1.Canvas 类下的所有 draw- 打头的方法，例如 drawCircle() drawBitmap()。
+         *         2.Paint 类的几个最常用的方法。具体是：
+         *            Paint.setStyle(Style style) 设置绘制模式
+         *            Paint.setColor(int color) 设置颜色
+         *            Paint.setStrokeWidth(float width) 设置线条宽度
+         *            Paint.setTextSize(float textSize) 设置文字大小
+         *            Paint.setAntiAlias(boolean aa) 设置抗锯齿开关
+         *
+         *3.
+         *      drawColor(Color.BLACK)    覆盖原来
+         *      drawColor(Color.parse())  设置遮罩层
+         *      drawRGB(int r, int g, int b) 和 drawARGB(int a, int r, int g, int b)
+         *      drawCircle(float centerX, float centerY, float radius, Paint paint) 画圆
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+
+
+    }
+}
